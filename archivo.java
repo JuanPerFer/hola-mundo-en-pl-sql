ba0d8440013d6e7d24a0959ee5d96fef03458ce6
Juan Pérez Fernández <juan.perez.fernandez.al@iespoligonosur.org>
2025-03-19 09:22:31 +0100

    Comentarios añadidos al código

---
+++ archivo.java
@@ -0,0 +1,10 @@
+/**
+ * Muestra por pantalla la frase "Hola mundo"
+ *
+ * @author Juan Pérez
+ */
+public class HolaMundo {
+	public static void main(String[] args) {
+		System.out.println("Hola mundo");
+	}
+}
